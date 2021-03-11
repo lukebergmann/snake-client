16633e3ec1cd8471ac32db31bafd60f803d8f8bb
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-11 02:18:50 +0000

    updated the control c code

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const { connect } = require('./client.js/client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+}
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.on('data', handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+
+setupInput();
+
+
+
+
