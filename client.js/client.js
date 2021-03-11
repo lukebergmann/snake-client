f6574ceeebafef7fb1c6d139b5a5d6a7a74df072
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-11 03:52:06 +0000

    updated constant file

--- client.js/client.js
+++ client.js/client.js
@@ -1,10 +1,10 @@
 const net = require('net');
-const { ip, port } = require('./constants');
+const { ip, port } = require('../constants');
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
+    host: ip,
+    port: port
   });
 
   // interpret incoming data as text
