ddac0d4734a15208a5ceea725a0b76c09b3e537b
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-11 03:34:20 +0000

    finished WASD keys

--- client.js/client.js
+++ client.js/client.js
@@ -10,7 +10,7 @@ const connect = function () {
   conn.setEncoding('utf8');
 
   conn.on('data', function (data) {
-    console.log('you ded cuz u idled');
+    console.log(data);
   })
   conn.on("connect", function () {
     console.log("Client is connected, it really is!")
@@ -21,51 +21,6 @@ const connect = function () {
   conn.on("connect", () => {
     conn.write("Move: left")
   });
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // }, 500);
-  // setTimeout(() => {
-  //   conn.write("Move: left");
-  // }, 1000);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // }, 1500);
-  // setTimeout(() => {
-  //   conn.write("Move: left");
-  // }, 2000);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // }, 2500);
-  // setTimeout(() => {
-  //   conn.write("Move: left");
-  // }, 3000);
-  //  setTimeout(() => {
-  //   conn.write("Move: up");
-  // }, 3500);
-  // setTimeout(() => {
-  //   conn.write("Move: left");
-  // }, 4000);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // }, 4500);
-  // setTimeout(() => {
-  //   conn.write("Move: left");
-  // }, 5000);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // }, 5500);
-  // setTimeout(() => {
-  //   conn.write("Move: left");
-  // }, 6000);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // }, 6500);
-  //  setTimeout(() => {
-  //   conn.write("Move: left");
-  // }, 7000);
-
-
-  
 
   return conn;
 }
--- play.js
+++ play.js
@@ -1,24 +1,15 @@
 const { connect } = require('./client.js/client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
-connect();
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-}
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.on('data', handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
 
 
-setupInput();
+let conn = connect();
+
+setupInput(conn);
+
+
+
+
 
 
 
